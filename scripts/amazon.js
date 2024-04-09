6986b7d0501ba48b0bc803c6e486acf0373b5d22
Mateo <mateo.benkovic648@gmail.com>
2024-04-09 20:37:07 +0200

    Added drop down quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -61,6 +61,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
     let matchingItem = '';
 
+    let dropDownQuantityString = document.querySelector(`.js-quantity-selector-${productId}`).value;
+
+    let dropDownQuantityNumber = Number(dropDownQuantityString);
+
+
     cart.forEach((item) => {
       if (productId === item.productId) {
         matchingItem = item;
@@ -68,11 +73,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += dropDownQuantityNumber;
     } else {
       cart.push({
       productId: productId,
-      quantity: 1
+      quantity: dropDownQuantityNumber
     });
     }
 
