e1eeb2001091a0e79200c81766b86d0124970501
Mateo <mateo.benkovic648@gmail.com>
2024-04-29 19:20:44 +0200

    Create oops version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 
