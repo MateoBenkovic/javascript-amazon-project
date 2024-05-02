301af26df0a92868d4395d0480178bcff004e5c3
Mateo <mateo.benkovic648@gmail.com>
2024-05-02 13:47:58 +0200

    Load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,13 +1,16 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/backend-practice.js';
+import { loadProducts } from "../data/products.js";
+//import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 //import "../../data/car.js";
 
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
 
-renderOrderSummary();
 
-renderPaymentSummary();
 
 
 
