bfece959cd39ef9649c9152b722893ac1b513f1d
Mateo <mateo.benkovic648@gmail.com>
2024-05-02 19:58:53 +0200

    Convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -111,6 +111,31 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'http://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance') {
+        return new Appliances(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+  });
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -123,7 +148,6 @@ export function loadProducts(fun) {
       }
       return new Product(productDetails);
     });
-    console.log('load products');
     fun();
   });
 
